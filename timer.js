329848e3d3c2f1e6188c14d4afe66df0b7968e3c
yangXiaoHan <540645270@qq.com>
2016-09-07 23:35:25 +0800

    add timer

---
+++ timer.js
@@ -0,0 +1,24 @@
+var timer = function (func, time){
+    var startTime = new Date().getTime(); 
+    var count = 0; 
+
+    var fixed = function () { 
+        count++; 
+        var offset = new Date().getTime() - (startTime + count * time); 
+        var nextTime = time - offset; 
+        if (nextTime < 0) {
+            nextTime = 0;
+        };
+        func();
+        setTimeout(fixed, nextTime); 
+    };
+    setTimeout(fixed, time); 
+};
+
+
+var index = function () {
+    console.log('123123123123123')''
+};
+
+
+timer(index, 1000);
